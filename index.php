f51fac2dd9f73de8d0ca6c9f0f93074a48adad18
kim0107 <kimslava0107@icloud.com>
2021-03-01 13:06:24 +0300

    rabota_s_usloviami

--- index.php
+++ index.php
@@ -27,9 +27,24 @@ $user_name = 'kim0107'; // укажите здесь ваше имя
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
-
+            <?php if($is_auth==1): ?>
+                <div class="user-menu__image">
+                    <img src="img/user.jpg" width="40" height="40" alt="Пользователь">
+                </div>
+            <div class="user-menu__logged">
+                <p></p>
+            </div>
+            <?php else: ?>
+            <ul class="user-menu__list">
+                <li class="user-menu__item">
+                    <a href="#">Регистрация</a>
+                </li>
+                <li class="user-menu__item">
+                    <a href="#">Вход</a>
+                </li>
+            </ul>
         <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
-
+            <?php endif; ?>
         </nav>
     </div>
 </header>
